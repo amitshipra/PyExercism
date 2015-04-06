3221517f336d1421657c81ed98720bb783fa5a47
Amit Gupta <amitg.ind@gmail.com>
2015-04-06 15:16:00 -0400

    Exercism: Start: CIPHER

---
+++ simple-cipher/cipher.py
@@ -0,0 +1,22 @@
+__author__ = 'agupt15'
+
+
+class Caesar:
+    def encode(self, plain_text):
+        return None
+
+    def decode(self, cryptic):
+        return None
+
+
+class Cipher:
+    def __init__(self, key=''):
+        self._key = key
+
+    def encode(self, plain_text):
+        return None
+
+    def decode(self, cryptic):
+        return None
+
+
