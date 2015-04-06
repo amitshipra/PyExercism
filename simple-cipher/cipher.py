88a3b5e959d1a379e71f437ecf3b42b87ab4cf86
Amit Gupta <amitg.ind@gmail.com>
2015-04-06 16:48:38 -0400

    Exercism: Start: CIPHER

--- simple-cipher/cipher.py
+++ simple-cipher/cipher.py
@@ -1,12 +1,31 @@
 __author__ = 'agupt15'
 
+import string
+
 
 class Caesar:
+    def __init__(self):
+        self.all_letters = string.ascii_lowercase
+
+    def get_cipher(self, ch):
+        ch = ch.lower()
+        if ch == ' ' or ch not in self.all_letters:
+            return ''
+        idx = self.all_letters.index(ch) + 3
+        if idx > len(self.all_letters):
+            idx = - 25
+        return self.all_letters[idx]
+
+    def get_plain(self, ch):
+        ch = ch.lower()
+        idx = self.all_letters.index(ch) - 3
+        return self.all_letters[idx]
+
     def encode(self, plain_text):
-        return None
+        return ''.join([self.get_cipher(x) for x in plain_text])
 
     def decode(self, cryptic):
-        return None
+        return ''.join([self.get_plain(x) for x in cryptic])
 
 
 class Cipher:
@@ -20,3 +39,4 @@ class Cipher:
         return None
 
 
+print(Caesar().decode('yhqlylglylfl'))
