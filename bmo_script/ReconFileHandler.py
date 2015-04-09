24201f25b8507de9ab000e45d7c7380d7850eb51
Amit Gupta <amitg.ind@gmail.com>
2015-04-09 17:04:15 -0400

    Trying the Script for File transfer.

--- bmo_script/ReconFileHandler.py
+++ bmo_script/ReconFileHandler.py
@@ -4,77 +4,14 @@ import os
 import shutil
 import datetime
 import fnmatch
+import argparse
 
 SYSTEMS = ['Rates', 'FX', 'Equities', 'Commodities']
 QUALIFIERS = ['SOURCE', 'DESTINATION']
 
-
-class FileHandler:
-    def __init__(self):
-        self.config = {}
-        for system in SYSTEMS:
-            self.config[system] = []
-
-    def read_config(self):
-        print('------Reading Config')
-        with open('recon-files.config') as config_file:
-            for line in config_file:
-                # Ignore comments and Blanks
-                if line.startswith('#') or line.strip() is '':
-                    continue
-                line = line.strip()
-                name, value = line.split('=')
-                system, qualifier = name.split('_')
-                self.config[system].append((value, system, qualifier))
-
-
-    def process(self):
-        self.read_config()
-        for system in self.config:
-            print("{} has values {}".format(system, self.config[system]))
-
-        for system in SYSTEMS:
-            properties = self.config[system]
-            if properties == [] or properties is None:
-                print("{} properties doesn't exists. Skipping".format(system))
-                continue
-            print("Working for {}".format(system))
-            source_folders = []
-            destination_folder = None
-            dtcc_folder = None
-            ok_filename = None
-            for file_folder, system, qualifier in properties:
-                if qualifier.startswith('SOURCE') is True:
-                    source_folders.append(file_folder)
-                elif qualifier.startswith('DESTINATION') is True:
-                    destination_folder = file_folder
-                elif qualifier.startswith('DTCC') is True:
-                    dtcc_folder = file_folder
-                elif qualifier.startswith('OKFILE') is True:
-                    ok_filename = file_folder
-                else:
-                    raise Exception('Unknown Property Qualifier {}'.format(qualifier))
-            print("Source Folders {} --> Destination [{}]".format(source_folders, destination_folder))
-
-            # transfer the XML files from source to destination
-            for source in source_folders:
-                for source_file in os.listdir(source):
-                    if not source_file.endswith('.xml'):
-                        continue
-                    source_file = os.path.join(source, source_file)
-                    shutil.move(source_file, destination_folder)
-                    print("{} moved to {}".format(source_file, destination_folder))
-
-            # create OK file in dtcc folder
-            for file in os.listdir(dtcc_folder):
-                if ok_filename is None:
-                    ok_filename = file + ".OK"
-                abs_file = os.path.join(dtcc_folder, ok_filename)
-                with open(abs_file, 'w') as f:
-                    f.write('')
-
-
 # # Utility methods
+DELIMITER = '||'
+DATE_PATTERN = 'YYYYMMDD'
 
 
 def pad_zero(x):
@@ -85,23 +22,22 @@ def pad_zero(x):
     return x
 
 
-DELIMITER = '||'
-DATE_PATTERN = 'YYYYMMDD'
+def get_formatted_date(date):
+    if date is None or date.strip() == '':
+        today = datetime.date.today()
+    return '{0}{1}{2}'.format(today.year, pad_zero(today.month), pad_zero(today.day))
 
 
 class AssetClassFileHandler:
     def __init__(self, date=None):
-        if date is None or date.strip() == '':
-            today = datetime.date.today()
-            date = '{0}{1}{2}'.format(today.year, pad_zero(today.month), pad_zero(today.day))
-        self.process_date = date
+        self.process_date = get_formatted_date(date)
         print('Working for date: {0}'.format(self.process_date))
         self.config = ConfigFile()
 
     def process(self, asset_class):
         asset_class = asset_class.strip()
         print("Processing Asset Class [{1}] for date: {0}".format(self.process_date, asset_class))
-        asset_class_working = 'Rates'
+        asset_class_working = 'FX'
         sources = self.config.get_properties_by_qualifier(asset_class_working, 'SOURCE')
         destination, ok_file = self.config.get_properties_by_qualifier(asset_class_working, 'DESTINATION')[0][1].split(
             DELIMITER)
@@ -120,35 +56,41 @@ class AssetClassFileHandler:
                 if fnmatch.fnmatch(file, src_pattern):
                     print('MATCH found for File: {0} Pattern {1}'.format(file, src_pattern))
                     src = os.path.join(src_path, file)
-                    if self.copyFile(src, file, destination, dest_pattern):
+                    if self.copy_file(src, file, destination, dest_pattern):
                         src_tuple[3] = True
                     break
 
-            for src_status in source_status:
-                print('Source {0} Status {1}'.format(src_status[0], src_status[3]))
+        if all([status[3] for status in source_status]):
+            # If all statuses are True - Generate the OK file
+            print('All transfers went OK - generating the OK FILE')
+            abs_file = os.path.join(destination, ok_file)
+            with open(abs_file, 'w') as f:
+                f.write('')
+            print('OK File Generated Successfully. {0} Asset class is completed'.format(asset_class))
 
 
-    def copyFile(self, src, src_file_name, dest, dest_pattern):
+    def copy_file(self, src, src_file_name, dest, dest_pattern):
         try:
             print('Trying to copy [{0} to [{1}]]'.format(src, dest))
             dest_file_name = src_file_name
             if DATE_PATTERN in dest_pattern:
                 dest_file_name = dest_pattern.replace(DATE_PATTERN, self.process_date)
-            if dest_pattern != 'SAME':
+            elif dest_pattern != 'SAME':
                 dest_file_name = dest_pattern
             print('Destination File Name: {0}'.format(dest_file_name))
 
-            shutil.copy2(src, dest)
-            #Rename the file
+            shutil.copy(src, dest)
+            # Rename the file
             dst_file = os.path.join(dest, src_file_name)
             new_dst_file_name = os.path.join(dest, dest_file_name)
             os.rename(dst_file, new_dst_file_name)
 
             print('File copied successfully')
             return True
-        except Exception:
+        except IOError:
             print('Could not process the file transfer/rename. OK File would not be generated')
             return False
+
 
 class ConfigFile:
     def __init__(self, file_name='recon-files.config'):
@@ -183,4 +125,22 @@ class ConfigFile:
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Processing Files transfer for Recon for Single or all asset classes for today or a particular date')
+
+    # ##
+    # Usage:
+    # >>  python ReconFileHandler.py -source Rates -date YYYYMMDD
+    # ##
+    parser.add_argument("-system", "--hostname", help="Asset Class from {Rates, FX, Commodities, Equities}",
+                        default='ALL')
+    default_dt = get_formatted_date()
+    parser.add_argument("-date", "--username", help="Date in YYYYMMDD format", default=default_dt)
+
+    args = parser.parse_args()
+
+    print("Asset Class {} Date ".format(
+        args.system,
+        args.date
+    ))
     AssetClassFileHandler().process('Rates')
