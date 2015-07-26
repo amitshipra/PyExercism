1d4391783a6cf101696f8e25ce677b40d22ee53d
amitshipra <amitg.ind@gmail.com>
2015-07-26 02:08:40 -0400

    Adding more problems - few solved - few unsolved. Try the secret-handshake one - Nice one.

---
+++ beer-song/beer.py
@@ -0,0 +1,9 @@
+__author__ = 'dias'
+
+def song(start, end=1):
+    return None
+
+def verse(counter):
+    return None
+
+TEMPLATE = "8 bottles of beer on the wall, 8 bottles of beer.\n" + "Take one down and pass it around, 7 bottles of beer on the wall.\n"
