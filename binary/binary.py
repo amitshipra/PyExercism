97c24c916c150a12c99fa0d19e04094005fcb510
amitshipra <amitg.ind@gmail.com>
2015-07-31 12:51:02 -0400

    Binary Completed.

--- binary/binary.py
+++ binary/binary.py
@@ -7,7 +7,7 @@ def parse_binary(str_num):
     if not is_valid(str_num):
         raise ValueError()
 
-    return int(sum([int(x) * math.pow(2, i) for i, x in enumerate(reversed(str_num))]))
+    return int(sum(int(x) * math.pow(2, i) for i, x in enumerate(reversed(str_num))))
 
 
 def is_valid(num):
