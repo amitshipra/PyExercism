89d38fc1ffef0bd9528060db37e77208d6e59b18
amitshipra <amitg.ind@gmail.com>
2015-07-31 12:48:51 -0400

    Binary Completed.

---
+++ binary/binary.py
@@ -0,0 +1,20 @@
+__author__ = 'dias'
+
+import math
+
+
+def parse_binary(str_num):
+    if not is_valid(str_num):
+        raise ValueError()
+
+    return int(sum([int(x) * math.pow(2, i) for i, x in enumerate(reversed(str_num))]))
+
+
+def is_valid(num):
+    for x in num:
+        if x != '1' and x != '0':
+            return False
+    return True
+
+
+print(parse_binary('1'))
